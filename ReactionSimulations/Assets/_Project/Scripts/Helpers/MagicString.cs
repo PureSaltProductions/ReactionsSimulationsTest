1d22123b16eba8adf532e28e89b5e755b73c7758
Anthony MIneo <anthony@hashbanginc.com>
2023-02-12 16:15:41 -0500

    Comments on MagicStrings

--- ReactionSimulations/Assets/_Project/Scripts/Helpers/MagicString.cs
+++ ReactionSimulations/Assets/_Project/Scripts/Helpers/MagicString.cs
@@ -1,13 +1,22 @@
 ﻿using Zenject;
 
+/// <summary>
+/// This is a helper class to store all the magic strings used in the project.
+/// </summary>
 public static class MagicString
 {
+    /// <summary>
+    /// Magic strings for Score data
+    /// </summary>
     public static class Score
     {
         public const string HighScore = "HighScore";
         public const string PreviousScore = "PreviousScore";
     }
 
+    /// <summary>
+    /// Magic strings for Scenes
+    /// </summary>
     public static class Scenes
     {
         public const string Initialization = "Initialization";
